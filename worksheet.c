77a28265f51e5c5566e45679a239113364738542
Hammouri3 <kkvc0229@leeds.ac.uk>
2026-05-04 22:05:24 +0000

    e

--- worksheet.c
+++ worksheet.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+/*
+ * Portfolio submission
+ * Name: Ahmad Hammouri
+ * ID: 201952967
+ */
+
 int add_values(int a, int b) {
     return a + b;
 }
@@ -31,7 +40,7 @@ double average(int *array, int length) {
         return 0.0;
     }
 
-    return (double)sum_array(array, length) / length;
+    return (double) sum_array(array, length) / length;
 }
 
 void find_max(int *array, int length, int *max_value) {
@@ -46,4 +55,77 @@ void find_max(int *array, int length, int *max_value) {
             *max_value = array[i];
         }
     }
+}
+
+/* Simple test runner */
+
+int test_add_values(void) {
+    return add_values(3, 4) == 7 &&
+           add_values(-2, 5) == 3 &&
+           add_values(0, 0) == 0;
+}
+
+int test_swap_values(void) {
+    int a = 3;
+    int b = 4;
+
+    swap_values(&a, &b);
+
+    return a == 4 && b == 3;
+}
+
+int test_sum_array(void) {
+    int values[] = {1, 2, 3, 4, 5};
+
+    return sum_array(values, 5) == 15;
+}
+
+int test_reverse_array(void) {
+    int values[] = {1, 2, 3, 4, 5};
+    int expected[] = {5, 4, 3, 2, 1};
+
+    reverse_array(values, 5);
+
+    for (int i = 0; i < 5; i++) {
+        if (values[i] != expected[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int test_average(void) {
+    int values[] = {1, 2, 3, 4, 5};
+    double result = average(values, 5);
+
+    return fabs(result - 3.0) < 0.0001;
+}
+
+int test_find_max(void) {
+    int values[] = {3, 9, 2, 7, 5};
+    int max_value = 0;
+
+    find_max(values, 5, &max_value);
+
+    return max_value == 9;
+}
+
+void print_result(const char *test_name, int passed) {
+    if (passed) {
+        printf("Test %s...       [ OK ]\n", test_name);
+    } else {
+        printf("Test %s...       [ FAILED ]\n", test_name);
+    }
+}
+
+int main(void) {
+    print_result("add_values", test_add_values());
+    print_result("swap_values", test_swap_values());
+    print_result("sum_array", test_sum_array());
+    print_result("reverse_array", test_reverse_array());
+    print_result("average", test_average());
+    print_result("find_max", test_find_max());
+
+    return 0;
 }
